a4a90a2561700d4dd1c4b4322654cc90cce5903c
ivan147792 <ivanctkbootcamp@gmail.com>
2025-04-30 16:32:59 +0200

    for loop for fizzBuzz

--- javaScript/operatori/script.js
+++ javaScript/operatori/script.js
@@ -33,4 +33,39 @@ const lista = ['Pero', 'Mika', 'Luka', 'Ana', 'Marija']
 // }
 
 // alert(poruka)
-alert(`Prvo ime: ${lista[0]}, zadnje ime: ${lista[lista.length - 1]}`)
+// alert(`Prvo ime: ${lista[0]}, zadnje ime: ${lista[lista.length -1]}`)
+const lista1 = []
+function fizzBuzz(index) {
+    if (index%3 === 0 && index%5 === 0) {
+        lista1.push('FizzBuzz')
+    }
+    else if (index % 3 === 0) {
+        lista1.push('Fizz')
+    
+    }
+    else if (index % 5 === 0) {
+        lista1.push('Buzz')
+    }
+    else {
+        lista1.push(index)
+    }
+}
+
+for (let i = 1; i < 101; i++) {
+    // if (i%3 === 0 && i%5 === 0) {
+    //     lista1.push('FizzBuzz')
+    // }
+    // else if (i % 3 === 0) {
+    //     lista1.push('Fizz')
+    
+    // }
+    // else if ( i % 5 === 0) {
+    //     lista1.push('Buzz')
+    // }
+    // else {
+    //     lista1.push(i)
+    // }
+    fizzBuzz(i)
+}
+
+console.log(lista1)
