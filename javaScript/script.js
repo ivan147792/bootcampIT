5e37803b3e5c81dc7d0f943b9ca7d7fca5d5a6ee
ivan147792 <ivanctkbootcamp@gmail.com>
2025-04-30 13:48:05 +0200

    kalkulator ostatka do 90. rodendana

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,11 +1,14 @@
-let name = prompt('unesi ime').trim()
-let limit = 20
-if (name.length >= limit) {
-    alert(name.slice(0, 20))
-}
-else if (!name) {
-    alert('Nemozemo dalje, nista nije uneseno')
-}
-else {
-    alert(`You have writen ${name.length} characters, you have ${limit-name.length} characters left!`)
-}
+let godine = prompt('unesi godine')
+godine= 90 - godine
+// if (name.length >= limit) {
+//     alert(name.slice(0, 20))
+// }
+// else if (!name) {
+//     alert('Nemozemo dalje, nista nije uneseno')
+// }
+// else {
+//     alert(`You have writen ${name.length} characters, you have ${limit-name.length} characters left!`)
+// }
+
+
+alert(`Do 90. rodendana preostaje ${godine} godina, ${godine*12} mjeseci, odnosno ${godine * 365} dana`);
