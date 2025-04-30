e6346597dbd89dae5e168f8fb681ab49911fc025
ivan147792 <ivanctkbootcamp@gmail.com>
2025-04-30 15:51:10 +0200

    lista metode

---
+++ javaScript/operatori/script.js
@@ -0,0 +1,36 @@
+// function alerti() {
+//     alert('Korak 1')
+//     alert('Korak 2')
+//     alert('Korak 3')
+// }
+
+// alerti()
+
+// const cijenaKruha = 1.7
+// let budzet = prompt('Unesi koliko novaca imas')
+
+// function kolikoKruha(novac, kruh) {
+//     let koliko = Math.floor(novac / kruh)
+//     let change = (novac%(koliko*kruh)).toFixed(2)
+    
+//     return `Za ${novac} para ${koliko} kruhova po 
+//         cijeni od ${kruh}. A ostalo je
+//         ${change} para.`
+// }
+
+// console.log(kolikoKruha(budzet, cijenaKruha))
+
+
+// let ime = prompt('Unesi ime')
+const lista = ['Pero', 'Mika', 'Luka', 'Ana', 'Marija']
+// let poruka = ''
+
+// if (lista.includes(ime)) {
+//     poruka = `Pozdrav ${ime}, dobrodosao/la nazad!`
+// }
+// else {
+//     poruka = 'Nazalost, nismo vas prepoznali!'
+// }
+
+// alert(poruka)
+alert(`Prvo ime: ${lista[0]}, zadnje ime: ${lista[lista.length - 1]}`)
